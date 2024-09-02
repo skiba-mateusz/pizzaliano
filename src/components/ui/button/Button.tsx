790fad5cfef563b239004cc630e367e9466b45b5
Mateusz Skiba <mateusz.skiba114@gmail.com>
2024-09-02 18:59:04 +0200

    refactor(button): refactor the button to work as a link

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,5 +1,6 @@
 import classNames from "classnames";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const variants = {
   filled: "bg-rose-500 text-neutral-50 focus:ring-2 ring-rose-300",
@@ -19,29 +20,38 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: keyof typeof sizes;
   className?: string;
   iconOnly?: boolean;
+  to?: string;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   variant = "filled",
   size = "medium",
   iconOnly = false,
+  to = "",
   className,
   children,
   ...restProps
 }) => {
+  const classes = classNames(
+    "p-[.6em_1.2em] flex items-center justify-center rounded-md outline-none",
+    variants[variant],
+    sizes[size],
+    className,
+    {
+      "aspect-square p-[.6em_.6em]": iconOnly,
+    }
+  );
+
+  if (to != "") {
+    return (
+      <Link className={classes} to={to}>
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <button
-      className={classNames(
-        "p-[.6em_1.2em] flex items-center justify-center rounded-md outline-none",
-        variants[variant],
-        sizes[size],
-        className,
-        {
-          "aspect-square p-[.6em_.6em]": iconOnly,
-        }
-      )}
-      {...restProps}
-    >
+    <button className={classes} {...restProps}>
       {children}
     </button>
   );
