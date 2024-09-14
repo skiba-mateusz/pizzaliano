cdabe9d909d79b4afb019214ac968c04a2d80b5c
Mateusz Skiba <mateusz.skiba114@gmail.com>
2024-09-14 14:32:28 +0200

    feat: Add animations with React-Transition-Group

--- src/components/ui/drawer/Drawer.tsx
+++ src/components/ui/drawer/Drawer.tsx
@@ -1,5 +1,6 @@
-import React, { useId } from "react";
+import React, { useId, useRef } from "react";
 import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
 import classNames from "classnames";
 import { DrawerProvider } from "./DrawerContext";
 
@@ -22,22 +23,34 @@ export const Drawer: React.FC<DrawerProps> = ({
   children,
 }) => {
   const titleID = useId();
-  if (!open) return null;
+  const ref = useRef<HTMLDivElement | null>(null);
 
   return createPortal(
     <DrawerProvider onClose={onClose} titleID={titleID}>
-      <div
-        className={classNames(
-          "fixed top-0 bottom-0 w-full sm:max-w-[32rem] flex flex-col bg-neutral-50 border border-neutral-300 z-20",
-          className,
-          position === "right" ? "right-0" : "left-0"
-        )}
-        role="dialog"
-        aria-modal="true"
-        aria-labelledby={titleID}
+      <CSSTransition
+        nodeRef={ref}
+        in={open}
+        classNames={classNames({
+          "drawer-right": position === "right",
+          "drawer-left": position === "left",
+        })}
+        timeout={300}
+        unmountOnExit
       >
-        {children}
-      </div>
+        <div
+          ref={ref}
+          className={classNames(
+            "fixed top-0 bottom-0 w-full sm:max-w-[32rem] flex flex-col bg-neutral-50 border border-neutral-300 z-20 origin-top",
+            className,
+            position === "right" ? "right-0" : "left-0"
+          )}
+          role="dialog"
+          aria-modal="true"
+          aria-labelledby={titleID}
+        >
+          {children}
+        </div>
+      </CSSTransition>
     </DrawerProvider>,
     document.body
   );
