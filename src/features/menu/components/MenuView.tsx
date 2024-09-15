66e4b5edda74902d9682fd7f0db89b4ccb603aa4
Mateusz Skiba <mateusz.skiba114@gmail.com>
2024-09-15 23:07:59 +0200

    feat: add animation when rendering menu

--- src/features/menu/components/MenuView.tsx
+++ src/features/menu/components/MenuView.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import classNames from "classnames";
 import { Loader } from "@/components/ui/loader/Loader";
 import { useMenuItems } from "../api/getMenuItems";
 import { GetMenuItemsParams } from "@/types/api";
@@ -16,8 +17,15 @@ export const MenuView: React.FC = () => {
     promotions: false,
   } as GetMenuItemsParams);
   const { categories, isLoading: isLoadingCategories } = useCategories();
-
   const [searchParams] = useSearchParams();
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    ref.current.classList.remove("translate-y-16");
+    ref.current.classList.remove("opacity-0");
+  }, [ref.current]);
 
   const categorySlugParam = searchParams.get("category");
   const isLoading = isLoadingMenuItems || isLoadingCategories;
@@ -25,24 +33,28 @@ export const MenuView: React.FC = () => {
   return !isLoading ? (
     <>
       <MenuCategories categories={categories} />
-      {categorySlugParam !== "promotions" ? (
-        Object.keys(categorizedMenuItems).map((category) => {
-          if (categorizedMenuItems[category].length > 0)
-            return (
-              <MenuSection
-                key={category}
-                category={category}
-                items={categorizedMenuItems[category]}
-              />
-            );
-        })
-      ) : (
-        <MenuSection
-          category="Promotions"
-          items={Object.values(categorizedMenuItems).flat()}
-        />
-      )}
-      )
+      <div
+        ref={ref}
+        className={classNames("translate-y-16 opacity-0 duration-500")}
+      >
+        {categorySlugParam !== "promotions" ? (
+          Object.keys(categorizedMenuItems).map((category) => {
+            if (categorizedMenuItems[category].length > 0)
+              return (
+                <MenuSection
+                  key={category}
+                  category={category}
+                  items={categorizedMenuItems[category]}
+                />
+              );
+          })
+        ) : (
+          <MenuSection
+            category="Promotions"
+            items={Object.values(categorizedMenuItems).flat()}
+          />
+        )}
+      </div>
     </>
   ) : (
     <Loader />
